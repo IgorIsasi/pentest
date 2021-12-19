ef0b87e4fd48a832698babb6580dab2115ecc3eb
Marru11 <46656741+Marru11@users.noreply.github.com>
2021-12-19 14:13:30 +0100

    Pasahitza egiaztatu hobetuta

--- kodea/app/config.php
+++ kodea/app/config.php
@@ -1,8 +1,8 @@
 <?php
     ob_start();
-    $hostname = "db"; //db con docker y localhost con XAMPP
-    $username = "admin"; //admin con docker y root con XAMPP
-    $password = "test"; //test con docker y "" con XAMPP
+    $hostname = "localhost"; //db con docker y localhost con XAMPP
+    $username = "root"; //admin con docker y root con XAMPP
+    $password = ""; //test con docker y "" con XAMPP
     $db = "database"; //Database en ambas
   
     $conn = mysqli_connect($hostname,$username,$password,$db);
